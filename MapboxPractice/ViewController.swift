1e3f47ff1a42481ff2aaa289738511e20cceb7d0
Mihai Ruber <mihairuber@Iulianas-MacBook-Pro.local>
2017-10-16 21:50:14 -0700

    second commit

--- MapboxPractice/ViewController.swift
+++ MapboxPractice/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let url = URL(string: "mapbox://styles/mapbox/streets-v10")
         let mapView = MGLMapView(frame: view.bounds, styleURL: url)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
         view.addSubview(mapView)
+        print('hello')
         // Do any additional setup after loading the view, typically from a nib.
     }
 
